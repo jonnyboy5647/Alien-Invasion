e5b7471a8b14691dc4ebb2f213839eb7e61362f9
Kamsumm07 <115957526+Kamsumm07@users.noreply.github.com>
2022-10-18 09:44:57 -0400

    Finished Lab 7

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the games settings."""
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (134, 178, 228)
