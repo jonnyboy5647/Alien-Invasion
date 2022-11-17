449df9798f192712a5e29d920a03c524ef4dd191
Kamsumm07 <115957526+Kamsumm07@users.noreply.github.com>
2022-11-16 21:14:39 -0500

    homework2

--- sideways.py
+++ sideways.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from pygame.sprite import Sprite
+from random import random
+from random import randint
 
 class Bullet(Sprite):
     def __init__(self, ss_game):
@@ -16,17 +18,58 @@ class Bullet(Sprite):
         self.rect.x = self.x
     def draw_bullet(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
+class Alien(Sprite):
+    def __init__(self, ss_game):
+        super().__init__()
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.image = pygame.image.load('images/alien.png')
+        self.rect = self.image.get_rect()
+        self.rect.left = self.screen.get_rect().right
+        alien_top_max = self.settings.screen_height - self.rect.height
+        self.rect.top = randint(0, alien_top_max)
+        self.x = float(self.rect.x)
+
+    def update(self):
+        self.x -= self.settings.alien_speed
+        self.rect.x = self.x
+class Bullet(Sprite):
+    def __init__(self, ss_game):
+        super().__init__()
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.color = self.settings.bullet_color
+        self.rect = pygame.Rect(0,0,self.settings.bullet_width,self.settings.bullet_height)
+        self.rect.midright = ss_game.ship.rect.midright
+        self.x = float(self.rect.x)
+    def update(self):
+        self.x += self.settings.bullet_speed
+        self.rect.x = self.x
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+class GameStats:
+    def __init__(self, ss_game):
+        self.settings = ss_game.settings
+        self.reset_stats()
+        self.game_active = True
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
+
+
 class Settings:
     def __init__(self):
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230,230,230)
+        self.bg_color = (255,255,255)
         self.ship_speed = 1.5
+        self.ship_limit = 3
         self.bullet_speed = 3.0
         self.bullet_width = 15
         self.bullet_height = 3.0
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
+        self.alien_speed = 1.0
+        self.alien_frequency = 0.002
 
 class Ship:
     def __init__(self, ss_game):
@@ -35,8 +78,7 @@ class Ship:
         self.screen_rect = ss_game.screen.get_rect()
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.rect.midleft = self.screen_rect.midleft
-        self.y = float(self.rect.y)
+        self.center_ship()
         self.moving_up = False
         self.moving_down = False
     def update(self):
@@ -45,6 +87,9 @@ class Ship:
         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.y += self.settings.ship_speed
         self.rect.y = self.y
+    def center_ship(self):
+        self.rect.midleft = self.screen_rect.midleft
+        self.y = float(self.rect.y)
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 class SidewaysShooter:
@@ -56,18 +101,51 @@ class SidewaysShooter:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Sideways Shooter")
         self.ship = Ship(self)
+        self.stats = GameStats(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
     def run_game(self):
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_aliens()
+                self._create_alien()
+                self._update_bullets()
             self._update_screen()
     def _update_bullets(self):
         self.bullets.update()
         for bullet in self.bullets.copy():
             if bullet.rect.left >= self.settings.screen_width:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    def _create_alien(self):
+        if random() < self.settings.alien_frequency:
+            alien = Alien(self)
+            self.aliens.add(alien)
+            print(len(self.aliens))
+    def _update_aliens(self):
+        self.aliens.update()
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        self._check_aliens_left_edge()
+    def _ship_hit(self):
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
+            self.aliens.empty()
+            self.bullets.empty()
+            self.ship.center_ship()
+        else:
+            self.stats.game_active = False
+    def _check_aliens_left_edge(self):
+        for alien in self.aliens.sprites():
+            if alien.rect.left < 0:
+                self._ship_hit()
+                break
+
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -102,7 +180,9 @@ class SidewaysShooter:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
+
 if __name__ == '__main__':
     ss = SidewaysShooter()
     ss.run_game()
