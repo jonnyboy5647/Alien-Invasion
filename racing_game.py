f4520b891a6d9013f00397b6cd87c8b4d44b4476
jonnyboy5647 <m253546@usna.edu>
2022-11-29 10:59:04 -0500

    added aliens

--- racing_game.py
+++ racing_game.py
@@ -5,7 +5,6 @@ from random import random
 from random import randint
 
 
-
 class Car:
     def __init__(self, rg_game):
         self.screen = rg_game.screen
@@ -18,6 +17,7 @@ class Car:
         self.moving_right = False
         self.moving_left = False
         self.center_car()
+
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.car_speed
@@ -30,20 +30,20 @@ class Car:
         self.rect.midleft = self.screen_rect.midleft
         self.y = float(self.rect.y)
 
-
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
 
 class Settings:
     def __init__(self):
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (0,0,255)
+        self.bg_color = (0, 0, 255)
         self.car_speed = 1.5
         self.bullet_speed = 3.0
         self.bullet_width = 4
         self.bullet_height = 15
-        self.bullet_color = (255,255,255)
+        self.bullet_color = (255, 255, 255)
         self.bullets_allowed = 10
         self.alien_speed = 1.0
         self.alien_frequency = 0.002
@@ -69,7 +69,7 @@ grass = pygame.image.load("images/grass.png")
 dirt = pygame.image.load("images/dirt.png")
 track = pygame.image.load("images/greentrack.png")
 
-soils = [grass,dirt,track]
+soils = [grass, dirt, track]
 
 # grab the dimension of our tile rectangle
 tile_rect = grass.get_rect()
@@ -81,15 +81,17 @@ def draw_background(bg_size):
     for r, grid_list in enumerate(grid):
         for c, grid_element in enumerate(grid_list):
             # blit the correc tile onto our screen
-            bg.blit(soils[grid_element], (c*TILE_SIZE, r*TILE_SIZE))
+            bg.blit(soils[grid_element], (c * TILE_SIZE, r * TILE_SIZE))
     return bg
+
+
 class Bullet(Sprite):
     def __init__(self, rg_game):
         super().__init__()
         self.screen = rg_game.screen
         self.settings = rg_game.settings
         self.color = self.settings.bullet_color
-        self.rect = pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midtop = rg_game.car.rect.midtop
         self.y = float(self.rect.y)
 
@@ -100,6 +102,7 @@ class Bullet(Sprite):
     def draw_bullet(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
 
+
 class Alien(Sprite):
     def __init__(self, ss_game):
         super().__init__()
@@ -107,28 +110,31 @@ class Alien(Sprite):
         self.settings = ss_game.settings
         self.image = pygame.image.load('images/aliens.png')
         self.rect = self.image.get_rect()
-        self.rect.left = self.screen.get_rect().right
-        alien_top_max = self.settings.screen_height - self.rect.height
-        self.rect.top = randint(0, alien_top_max)
-        self.x = float(self.rect.x)
+        self.rect.bottom = self.screen.get_rect().top
+        alien_top_max = self.settings.screen_width - self.rect.width
+        self.rect.left = randint(0, alien_top_max)
+        self.y = float(self.rect.y)
 
     def update(self):
-        self.x -= self.settings.alien_speed
-        self.rect.x = self.x
+        self.y += self.settings.alien_speed
+        self.rect.y = self.y
+
 
 class GameStats:
     def __init__(self, ss_game):
         self.settings = ss_game.settings
         self.reset_stats()
         self.game_active = True
+
     def reset_stats(self):
         self.cars_left = self.settings.car_limit
+
 
 class RacingGame:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Racing Game")
@@ -140,11 +146,12 @@ class RacingGame:
     def run_game(self):
         while True:
             self._check_events()
-            self.car.update()
-            self._update_bullets()
+            if self.stats.game_active:
+                self.car.update()
+                self._update_bullets()
+                self._update_aliens()
+                self._create_alien()
             self._update_screen()
-            self._update_aliens()
-            self._create_alien()
 
     def _check_aliens_left_edge(self):
         for alien in self.aliens.sprites():
@@ -166,6 +173,7 @@ class RacingGame:
             alien = Alien(self)
             self.aliens.add(alien)
             print(len(self.aliens))
+
     def _update_aliens(self):
         self.aliens.update()
         if pygame.sprite.spritecollideany(self.car, self.aliens):
@@ -181,7 +189,7 @@ class RacingGame:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
-    def _check_keydown_events(self,event):
+    def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.car.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -191,7 +199,7 @@ class RacingGame:
         elif event.key == pygame.K_q:
             sys.exit()
 
-    def _check_keyup_events(self,event):
+    def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.car.moving_right = False
         elif event.key == pygame.K_LEFT:
@@ -215,24 +223,6 @@ class RacingGame:
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
         pygame.display.flip()
-
-class Alien(Sprite):
-    def __init__(self, ss_game):
-        super().__init__()
-        self.screen = ss_game.screen
-        self.settings = ss_game.settings
-        self.image = pygame.image.load('images/aliens.png')
-        self.rect = self.image.get_rect()
-        self.rect.bottom = self.screen.get_rect().top
-        alien_top_max = self.settings.screen_width - self.rect.width
-        self.rect.right = randint(0, alien_top_max)
-        self.y = float(self.rect.y)
-
-    def update(self):
-        self.y -= self.settings.alien_speed
-        self.rect.y = self.y
-
-
 
 
 if __name__ == '__main__':
